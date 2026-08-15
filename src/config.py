5021dd4087a00a847426a5d3c5266f60a1c33610
Gautam Mishra <g20autammishra1234@gmail.com>
2026-08-15 14:07:15 +0530

    commit config file

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 RAW_DATA = os.path.join(BASE_DIR, 'data', 'raw', 'online_retail_II.csv')
 INTERIM_DATA = os.path.join(BASE_DIR, 'data', 'interim', 'retail_interim.csv')
 PROCESSED_DATA = os.path.join(BASE_DIR, 'data', 'processed', 'reatil_processed.csv')
+CUSTOMER_FEATURES = os.path.join(BASE_DIR, "data", "processed", "customer_features.csv")
 
 ARTIFACT_DIR = os.path.join(BASE_DIR, 'artifacts')
 MODEL_DIR = os.path.join(BASE_DIR, 'models')
